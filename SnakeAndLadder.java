876d960bf23e6dca561657c16585ebac21d72fa9
Lagashettik <Lagashettik@gmail.com>
2021-01-24 15:47:22 +0530

    Added: Player Start Position Zero

--- SnakeAndLadder.java
+++ SnakeAndLadder.java
@@ -5,7 +5,14 @@ public class SnakeAndLadder {
 	System.out.println("Snake and Ladder game played with single player at start position 0");
 	}
 
+	Game(){
+		int PlayerPosition=0;
+		int StartPosition=0;
+	}
+
 	public static void main(String args[]) {
 	SnakeAndLadder Snake=new SnakeAndLadder();
+
+	Snake.Game();
 	}
 }
