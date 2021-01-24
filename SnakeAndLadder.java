1f49048eab8806c89976e0d63fda2468269ca56d
Lagashettik <Lagashettik@gmail.com>
2021-01-24 17:15:38 +0530

    Modified: Player plays until Wins

--- SnakeAndLadder.java
+++ SnakeAndLadder.java
@@ -7,34 +7,34 @@ public class SnakeAndLadder {
 
 	void Game(){
 		int PlayerPosition=0;
-		int StartPosition=0;
 
-		int RollDice=(int) ( (Math.floor (Math.random() * 10) % 6) + 1);
-		System.out.println("The Dice Roll is "+RollDice);
+		while (PlayerPosition < 100){
+			int RollDice=(int) ( (Math.floor (Math.random() * 10) % 6) + 1);
+			System.out.println("The Dice Roll is "+RollDice);
 
-		int PlayerCheck=(int) Math.floor(Math.random() * 10) % 3;
-		// PlayerCheck= 0 Then Player goes down by Snake
-		// PlayerCheck= 1 Then Player  Not Play
-		// PlayerCheck= 2 Then Player climb Ladder
-		switch (PlayerCheck) {
-			case 0:
-					System.out.println("SNAKE");
-					PlayerPosition-=RollDice;
-					break;
-			case 1:
-					System.out.println("No Play");
-					break;
-			case 2:
-					System.out.println("LADDER");
-					PlayerPosition+=RollDice;
-					break;
+			int PlayerCheck=(int) Math.floor(Math.random() * 10) % 3;
+			// PlayerCheck= 0 Then Player goes down by Snake
+			// PlayerCheck= 1 Then Player  Not Play
+			// PlayerCheck= 2 Then Player climb Ladder
+			switch (PlayerCheck) {
+				case 0:
+						System.out.println("SNAKE");
+						PlayerPosition-=RollDice;
+						break;
+				case 1:
+						System.out.println("No Play");
+						break;
+				case 2:
+						System.out.println("LADDER");
+						PlayerPosition+=RollDice;
+						break;
+					}
+			if(PlayerPosition < 0)
+					PlayerPosition=0;
+
+			System.out.println("Player Position is "+PlayerPosition);
 		}
-		if(PlayerPosition < 0)
-				PlayerPosition=0;
-
-		System.out.println("Player Position is "+PlayerPosition);
 	}
-
 	public static void main(String args[]) {
 	SnakeAndLadder Snake=new SnakeAndLadder();
 
