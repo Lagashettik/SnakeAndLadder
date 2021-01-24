74fd55fb3e0d29d572367715b06c5252a587f22d
Lagashettik <Lagashettik@gmail.com>
2021-01-24 15:33:54 +0530

    Started Game with single player start position 0

---
+++ SnakeAndLadder.java
@@ -0,0 +1,11 @@
+
+public class SnakeAndLadder {
+
+	SnakeAndLadder() {
+	System.out.println("Snake and Ladder game played with single player at start position 0");
+	}
+
+	public static void main(String args[]) {
+	SnakeAndLadder Snake=new SnakeAndLadder();
+	}
+}
